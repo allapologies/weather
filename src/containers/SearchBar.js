75076e92d175ca15360f0a5b9fb88c6780701e5b
alexvtec <alx.panteleev@gmail.com>
2016-03-25 19:13:06 +0300

    search bar container

---
+++ src/containers/SearchBar.js
@@ -0,0 +1,15 @@
+'use strict';
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  
+  render() {
+    return (
+      <form className='input-group'>
+        <input />
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
